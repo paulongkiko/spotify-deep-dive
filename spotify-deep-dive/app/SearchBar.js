570de67aaddc0d29cfc795c4619b01d4dabb71d0
Paul Ongkiko <paul.ongkiko@slu.edu>
2023-07-30 00:08:05 -0500

    made search bar

---
+++ spotify-deep-dive/app/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBar = ({ onChange, onEnterPress }) => {
+    const handleKeyDown = (event) => {
+        if (event.key == 'Enter') {
+            onEnterPress(event.target.value);
+        }
+    }
+    return (
+        <div className="flex items-center justify-center">
+            <input
+            type = "text"
+            placeholder = "Search for an artist..."
+            className="py-2 px-4 border border-gray-300 rounded-lg shadow-sm focus:ring focus:ring-blue-200 focus:outline-none"
+            onChange = {onChange}
+            onKeyDown = {handleKeyDown}
+            />
+        </div>
+    );
+};
+
+export default SearchBar;
